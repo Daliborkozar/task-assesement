aa734ec467177afabd13fc793021f88413951ef3
dalibork <kozardalibor@gmail.com>
2022-02-11 16:52:10 +0100

    product details description, structure for pricing and details

--- src/components/product/ProductPricing.js
+++ src/components/product/ProductPricing.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
+import { Grid, Box, Divider, Stack, Chip, Typography } from "@mui/material";
+import { TitleComponent } from "../UI/TitleComponent";
+
+const Wrapper = styled(Box)({
+  backgroundColor: "#FFFFFF",
+  padding: 20,
+  width: "100%",
+  minWidth: 360,
+  maxWidth: 600,
+
+});
+
+const TitleContainer = styled(Grid)({
+  marginBottom: 10,
+})
 
 export const ProductPricing = () => {
   return (
-    <div>ProductPricing</div>
-  )
-}
+    <Wrapper >
+      <Box>
+        <TitleContainer container>
+          <Grid item >
+            <TitleComponent>PRICING & SHIPPING</TitleComponent>
+          </Grid>
+        </TitleContainer>
+      </Box>
+      <Divider />
+      <Box>
+      <Typography  variant="body1" color="#A9A9A9">
+          Features
+        </Typography>
+        
+      </Box>
+      <Box>
+      <Typography  variant="body1" color="#A9A9A9">
+          Attachements
+        </Typography>
+      </Box>
+      <Box>
+        <Typography  variant="body1" color="#A9A9A9">
+          Keywords
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Extra Soft" color="primary" />
+          <Chip color="primary" label="Soft" />
+          <Chip label="Medium" color="primary"/>
+          <Chip label="Hard" color="primary"/>
+        </Stack>
+      </Box>
+    </Wrapper>
+  );
+};
